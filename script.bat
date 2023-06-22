3ed913dbe6ed2194faa0efe8fa20acd606d48c44
menegass0 <124715996+menegass0@users.noreply.github.com>
2023-06-22 14:49:33 -0300

    Update script.bat

--- script.bat
+++ script.bat
@@ -1,2 +1,2 @@
 @echo off
-schtasks /create /sc minute /mo 5 /tn "jupiter-dtgi" /tr "C:\Users\%USERNAME%\AppData\Local\Programs\jupiter-script\jupiter-script.vbs" /st 00:00
+schtasks /create /sc hourly /mo 2 /tn "jupiter-dtgi" /tr "C:\Users\%USERNAME%\AppData\Local\Programs\jupiter-script\jupiter-script.vbs" /st 00:00
